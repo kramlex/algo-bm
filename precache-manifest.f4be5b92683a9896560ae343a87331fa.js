dd8d260c5ed3af845609c79db6763c7d1f664b2f
kramlex <mark.dubkov@gmail.com>
2020-10-14 14:03:09 +0700

    Updates

---
+++ precache-manifest.f4be5b92683a9896560ae343a87331fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94d191f4f3c45fd3abb94005a79ff971",
+    "url": "/algo-bm/index.html"
+  },
+  {
+    "revision": "0d4270f56a699e6b8aed",
+    "url": "/algo-bm/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "dc0e119591e14c7afb7a",
+    "url": "/algo-bm/static/css/main.2f8ad58a.chunk.css"
+  },
+  {
+    "revision": "0d4270f56a699e6b8aed",
+    "url": "/algo-bm/static/js/2.1bc0713c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/algo-bm/static/js/2.1bc0713c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc0e119591e14c7afb7a",
+    "url": "/algo-bm/static/js/main.6b9b522a.chunk.js"
+  },
+  {
+    "revision": "deb5041b33be0fa3eefa",
+    "url": "/algo-bm/static/js/runtime-main.a6bc3cdf.js"
+  }
+]);
